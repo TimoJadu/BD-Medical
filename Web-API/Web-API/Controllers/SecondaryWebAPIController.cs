d71166fe48c8ca4d1fdc2dac82ecee0842cc372b
TimoJadu <subhajit.ece.ind@gmail.com>
2018-08-28 05:42:55 +0100

    First commit
    First commit

---
+++ Web-API/Web-API/Controllers/SecondaryWebAPIController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+//using System.Web.Mvc;
+using System.Web.Http.Cors;
+
+namespace Web_API.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class SecondaryWebAPIController : ApiController
+    {
+        Random rdNumber = new Random();
+        Random rdDelay = new Random();
+        Random rdMultiplier = new Random();
+
+        
+        [ActionName("Generator")]
+        [HttpGet]
+        public string Generator([FromUri]int Y)
+        {
+            int[] arrY = new Int32[Y];           
+
+            int i = -1;
+            do
+            {
+                ++i;
+                //Thread.Sleep(rdDelay.Next(5, 10) * 1000);
+                return (rdNumber.Next(1, 100)).ToString();
+
+            } while (i < Y);
+        }
+
+        
+        [ActionName("Multiplier")]
+        [HttpGet]
+        public string Multiplier([FromUri] int givenBatchNumber)
+        {
+            int number = givenBatchNumber * rdMultiplier.Next(2, 4);
+            //Thread.Sleep(rdDelay.Next(5, 10) * 1000);
+
+            return (number).ToString();
+        }
+    }
+}
---
+++ Web-API/Web-API/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web_API.Controllers
+{
+    //[Authorize]
+    public class ValuesController : ApiController
+    {
+        static List<string> listStatic = new List<string>() {
+            "value0", "value1","value2"
+        };
+        
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return listStatic;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return listStatic[id];
+
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            listStatic.Add(value);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+            listStatic[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            listStatic.RemoveAt(id);
+        }
+    }
+}
